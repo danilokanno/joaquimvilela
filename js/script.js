05eea279c79e70f9ed0bf0592a118f78b530e4ae
danilokanno <danilokanno7@gmail.com>
2026-03-31 08:38:00 -0300

    versão para celular

--- js/script.js
+++ js/script.js
@@ -24,3 +24,13 @@ nextBtn.addEventListener('click', nextSlide);
 prevBtn.addEventListener('click', prevSlide);
 
 setInterval(nextSlide, 5000);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
